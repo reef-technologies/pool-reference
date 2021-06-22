874adcac9dcbdbcd2b469b1245c2397bae0f759b
Michał Nowacki <michal.nowacki@reef.pl>
2021-06-22 16:07:34 +0200

    allow the server to be run as a module and fix imports

--- pool/pool.py
+++ pool/pool.py
@@ -43,11 +43,11 @@ from chia.pools.pool_puzzles import (
     launcher_id_to_p2_puzzle_hash,
 )
 
-from difficulty_adjustment import get_new_difficulty
-from singleton import create_absorb_transaction, get_and_validate_singleton_state_inner, get_coin_spend
-from store import PoolStore
-from record import FarmerRecord
-from util import error_dict
+from .difficulty_adjustment import get_new_difficulty
+from .singleton import create_absorb_transaction, get_and_validate_singleton_state_inner, get_coin_spend
+from .store import PoolStore
+from .record import FarmerRecord
+from .util import error_dict
 
 
 class Pool:
